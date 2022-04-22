b43ae87877051dd6d586006d64bf000bbc915b76
Caitlyn Maier <98415962+cmaier13@users.noreply.github.com>
2022-04-22 13:58:41 +0000

    test 1 complete

---
+++ Surface.py
@@ -0,0 +1,10 @@
+from Rectangle import Rectangle 
+
+class Surface: 
+  def __init__(self, filename, x, y, h, w):
+    self.rect = Rectangle(x,y,h,w)
+    self.image = filename  
+
+  def getrect(self):
+    self.rect = rectangle
+    return rectangle 
