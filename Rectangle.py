c1372b979bbb7850948b5def7cb5fb7c85cb5b94
Caitlyn Maier <98415962+cmaier13@users.noreply.github.com>
2022-04-22 14:05:55 +0000

    added docstrings

--- Rectangle.py
+++ Rectangle.py
@@ -5,6 +5,10 @@ class Rectangle:
     self.height = max(0, h)
     self.width = max(0, w)
 
+  '''takes the parameters x, y, h, and w and assigns them to variables'''
+
   def __str__(self):
     string = "(x: {self.x}, y: {self.y}) width: {self.width}, height: {self.height}"
     return string
+
+'''creates a string using variables from the init method and returns the string'''
--- Surface.py
+++ Surface.py
@@ -5,5 +5,9 @@ class Surface:
     self.rect = Rectangle(x,y,h,w)
     self.image = filename  
 
+'''takes the parameters filename, x, y, h, w and assigns filename to a variable and uses the remaining parameters to create a rectangle object'''
+  
   def getRect(self):
     return self.rect 
+
+'''uses the rectangle object from init and returns the rectangle object '''
